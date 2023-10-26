85c9a49c8c22e3f2b6f85b422e5cc19a3ef2b10c
Oleksandr Serhan <oleksandr.serhan@intellectsoft.net>
2023-10-26 15:00:23 +0300

    Added nyan reporter to jest and cypress;

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'cypress';
 
+import cypressWatchAndReload from 'cypress-watch-and-reload/plugins';
+
 export default defineConfig({
   e2e: {
+    // env: {
+    //   'cypress-watch-and-reload': {
+    //     watch: ['src/*', 'circle.yml'],
+    //   },
+    // },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
+      return cypressWatchAndReload(on, config);
     },
   },
   reporter: 'nyan',
