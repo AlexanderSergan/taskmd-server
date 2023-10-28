891223b8a9bc5dfc523c012785c7367f40972089
Oleksandr Serhan <oleksandr.serhan@intellectsoft.net>
2023-10-28 16:12:59 +0300

    Connected to MongoDB; Cats schema;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import 'dotenv/config';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
+    AuthModule,
+    CatsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
