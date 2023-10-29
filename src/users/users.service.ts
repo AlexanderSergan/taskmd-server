91d5c583351b77ff1625fdd8b9b49f24c7d8d53d
Oleksandr Serhan <oleksandr.serhan@intellectsoft.net>
2023-10-29 18:11:22 +0200

    UserModel injected into UserService;

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { User } from '../schemas/user.schema'
+import { Model } from 'mongoose'
 
 @Injectable()
 export class UsersService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
   private readonly users = [
     {
       userId: 1,
@@ -16,6 +21,6 @@ export class UsersService {
   ]
 
   async findOne(username: string): Promise<any> {
-    return this.users.find((user) => user.username === username)
+    return this.users.find(user => user.username === username)
   }
 }
