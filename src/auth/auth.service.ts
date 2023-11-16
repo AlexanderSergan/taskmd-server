a228473a05341dc10cd94cb89084f13176ae9c42
Oleksandr Serhan <oleksandr.serhan@intellectsoft.net>
2023-11-16 19:47:23 +0200

    Some CORS fixes;

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
 import { InjectModel } from '@nestjs/mongoose'
+import { Model } from 'mongoose'
 import { UsersService } from 'users/users.service'
 import { User } from '../schemas/user.schema'
-import { Model } from 'mongoose'
-import { JwtService } from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  })
+  const app = await NestFactory.create(AppModule)
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:4200', 'http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }
+  app.enableCors(corsOptions)
 
   app.select(AppModule).get(AppModule).onModuleInit()
 
